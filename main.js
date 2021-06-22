4e2d9b3904e867a45d7b407abbc5d5c06ae85dc9
randy525 <69539478+randy525@users.noreply.github.com>
2021-06-22 15:37:34 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+
+const score = document.querySelector('.score'),
+   start = document.querySelector('.start'),
+   gameArea = document.querySelector('.gameArea'),
+   car = document.createElement('div');
+car.classList.add('car');
+
+const keys = {
+   ArrowUp: false,
+   ArrowRight: false,
+   ArrowDown: false,
+   ArrowLeft: false,
+};
+
+const setting = {
+   start: false,
+   score: 0,
+   speed: 3
+};
+
+
+function playGame() {
+   console.log('play game');
+   if(setting.start) {
+      requestAnimationFrame(playGame);
+   }
+}
+
+function startGame() {
+   start.classList.add('hide');
+   setting.start = true;
+   gameArea.appendChild(car);
+   requestAnimationFrame(playGame);
+}
+
+function startRun(event) {
+   event.preventDefault();
+   keys[event.key] = true;
+}
+
+function stopRun(event) {
+   event.preventDefault();
+   keys[event.key] = false;
+}
+
+
+start.addEventListener('click', startGame);
+document.addEventListener('keydown', startRun);
+document.addEventListener('keyup', stopRun);
+
